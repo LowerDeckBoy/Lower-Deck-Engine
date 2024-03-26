71b8c85a56bd6f1fb221db0949c734025db53a53
LowerDeckBoy <pa.krawiec@onet.pl>
2024-03-26 18:55:12 +0100

    Initial rework

--- Source/Engine/RHI/D3D12/D3D12RootSignature.cpp
+++ Source/Engine/RHI/D3D12/D3D12RootSignature.cpp
@@ -1,64 +1,127 @@
 #include <AgilitySDK/d3d12.h>
-#include <AgilitySDK/d3dx12/d3dx12.h>
 #include "D3D12RootSignature.hpp"
 #include "D3D12Device.hpp"
 #include "D3D12Utility.hpp"
 
 namespace lde::RHI
 {
-	D3D12RootSignature::D3D12RootSignature(D3D12Device* pDevice,
-		const std::span<CD3DX12_ROOT_PARAMETER1>& Parameters,
-		const std::span<D3D12_STATIC_SAMPLER_DESC>& Samplers,
-		const D3D12_ROOT_SIGNATURE_FLAGS& RootFlags,
-		LPCWSTR DebugName)
-	{
-		Create(pDevice, Parameters, Samplers, RootFlags, DebugName);
-	}
-	
 	D3D12RootSignature::~D3D12RootSignature()
 	{
 		Release();
 	}
-	
-	void D3D12RootSignature::Create(D3D12Device* pDevice,
-		const std::span<CD3DX12_ROOT_PARAMETER1>& Parameters,
-		const std::span<D3D12_STATIC_SAMPLER_DESC>& Samplers,
-		const D3D12_ROOT_SIGNATURE_FLAGS& RootFlags,
-		LPCWSTR DebugName)
+
+	void D3D12RootSignature::AddConstants(uint32 Count, uint32 RegisterSlot, uint32 Space, D3D12_SHADER_VISIBILITY Visibility)
 	{
-		CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC rootDesc{};
-		rootDesc.Init_1_1(
-			static_cast<uint32>(Parameters.size()), Parameters.data(),
-			static_cast<uint32>(Samplers.size()), Samplers.data(),
-			RootFlags);
-	
-		Serialize(pDevice, rootDesc, DebugName);
+		D3D12_ROOT_PARAMETER1 parameter{};
+		parameter.ParameterType = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS;
+		parameter.Constants.Num32BitValues = Count;
+		parameter.Constants.ShaderRegister = RegisterSlot;
+		parameter.Constants.RegisterSpace = Space;
+		parameter.ShaderVisibility = Visibility;
+
+		m_Parameters.emplace_back(parameter);
 	}
-	
+
+	void D3D12RootSignature::AddCBV(uint32 RegisterSlot, uint32 Space, D3D12_SHADER_VISIBILITY Visibility)
+	{
+		D3D12_ROOT_PARAMETER1 parameter{};
+		parameter.ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+		parameter.Descriptor.ShaderRegister = RegisterSlot;
+		parameter.Descriptor.RegisterSpace = Space;
+		parameter.Descriptor.Flags = D3D12_ROOT_DESCRIPTOR_FLAG_DATA_STATIC_WHILE_SET_AT_EXECUTE;
+		parameter.ShaderVisibility = Visibility;
+
+		m_Parameters.emplace_back(parameter);
+	}
+
+	void D3D12RootSignature::AddSRV(uint32 RegisterSlot, uint32 Space, D3D12_SHADER_VISIBILITY Visibility)
+	{
+		D3D12_ROOT_PARAMETER1 parameter{};
+		parameter.ParameterType = D3D12_ROOT_PARAMETER_TYPE_SRV;
+		parameter.Descriptor.ShaderRegister = RegisterSlot;
+		parameter.Descriptor.RegisterSpace = Space;
+		parameter.Descriptor.Flags = D3D12_ROOT_DESCRIPTOR_FLAG_DATA_STATIC_WHILE_SET_AT_EXECUTE;
+		parameter.ShaderVisibility = Visibility;
+
+		m_Parameters.emplace_back(parameter);
+	}
+
+	void D3D12RootSignature::AddUAV(uint32 RegisterSlot, uint32 Space, D3D12_SHADER_VISIBILITY Visibility)
+	{
+		D3D12_ROOT_PARAMETER1 parameter{};
+		parameter.ParameterType = D3D12_ROOT_PARAMETER_TYPE_UAV;
+		parameter.Descriptor.ShaderRegister = RegisterSlot;
+		parameter.Descriptor.RegisterSpace = Space;
+		parameter.Descriptor.Flags = D3D12_ROOT_DESCRIPTOR_FLAG_DATA_STATIC_WHILE_SET_AT_EXECUTE;
+		parameter.ShaderVisibility = Visibility;
+
+		m_Parameters.emplace_back(parameter);
+	}
+
+	void D3D12RootSignature::AddStaticSampler(uint32 RegisterSlot, uint32 Space, D3D12_FILTER Filter, D3D12_TEXTURE_ADDRESS_MODE AddressMode, D3D12_COMPARISON_FUNC ComparsionFunc)
+	{
+		D3D12_STATIC_SAMPLER_DESC1 sampler{};
+		sampler.ShaderRegister = RegisterSlot;
+		sampler.RegisterSpace = Space;
+		sampler.Filter = Filter;
+		sampler.AddressU = AddressMode;
+		sampler.AddressV = AddressMode;
+		sampler.AddressW = AddressMode;
+		sampler.ComparisonFunc = ComparsionFunc;
+		sampler.BorderColor = D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK;
+		sampler.MinLOD = 0.0f;
+		sampler.MaxLOD = D3D12_FLOAT32_MAX;
+		sampler.MaxAnisotropy  = D3D12_MAX_MAXANISOTROPY;
+		sampler.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+		sampler.Flags = D3D12_SAMPLER_FLAG_NONE;
+
+		m_StaticSamplers.emplace_back(sampler);
+	}
+
+	HRESULT D3D12RootSignature::Build(D3D12Device* pDevice, PipelineType eType, std::string DebugName)
+	{
+		const D3D12_ROOT_SIGNATURE_FLAGS flags = 
+			D3D12_ROOT_SIGNATURE_FLAG_CBV_SRV_UAV_HEAP_DIRECTLY_INDEXED | 
+			D3D12_ROOT_SIGNATURE_FLAG_DENY_DOMAIN_SHADER_ROOT_ACCESS | 
+			D3D12_ROOT_SIGNATURE_FLAG_DENY_GEOMETRY_SHADER_ROOT_ACCESS | 
+			D3D12_ROOT_SIGNATURE_FLAG_DENY_HULL_SHADER_ROOT_ACCESS;
+
+		D3D12_VERSIONED_ROOT_SIGNATURE_DESC desc{};
+		desc.Desc_1_2.NumParameters = static_cast<uint32>(m_Parameters.size());
+		desc.Desc_1_2.pParameters = m_Parameters.data();
+		desc.Desc_1_2.NumStaticSamplers = static_cast<uint32>(m_StaticSamplers.size());
+		desc.Desc_1_2.pStaticSamplers = m_StaticSamplers.data();
+		desc.Desc_1_2.Flags = flags;
+		desc.Version = D3D_ROOT_SIGNATURE_VERSION_1_2;
+
+		Type = eType;
+
+		ID3DBlob* signature = nullptr;
+		ID3DBlob* error = nullptr;
+
+		DX_CALL(D3D12SerializeVersionedRootSignature(&desc, &signature, &error), "Failed to serialize RootSignature!");
+		
+		if (error)
+		{
+			std::string message = std::format("Root Signature building error:\n, {}", (char*)error->GetBufferPointer());
+			::MessageBoxA(nullptr, message.c_str(), "D3D12 Error", MB_OK);
+			throw std::exception();
+		}
+
+		HRESULT result = pDevice->GetDevice()->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&m_RootSignature));
+		DX_CALL(result);
+
+		if (!DebugName.empty())
+		{
+			m_RootSignature->SetName(String::ToWide(DebugName).c_str());
+		}
+		
+		return result;
+	}
+
 	void D3D12RootSignature::Release()
 	{
 		SAFE_RELEASE(m_RootSignature);
 	}
 	
-	void D3D12RootSignature::Serialize(D3D12Device* pDevice, const CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC& Desc, LPCWSTR DebugName)
-	{
-		Ref<ID3DBlob> signature;
-		Ref<ID3DBlob> error;
-	
-		DX_CALL(D3DX12SerializeVersionedRootSignature(
-			&Desc,
-			D3D_ROOT_SIGNATURE_VERSION_1_1,
-			&signature, &error));
-	
-		DX_CALL(pDevice->GetDevice()->CreateRootSignature(
-			0,
-			signature->GetBufferPointer(), signature->GetBufferSize(),
-			IID_PPV_ARGS(&m_RootSignature)));
-	
-		if (DebugName)
-			m_RootSignature.Get()->SetName(DebugName);
-	
-		SAFE_RELEASE(signature);
-		SAFE_RELEASE(error);
-	}
 } // namespace lde::RHI
--- Source/Engine/RHI/D3D12/D3D12RootSignature.hpp
+++ Source/Engine/RHI/D3D12/D3D12RootSignature.hpp
@@ -5,6 +5,7 @@
 */
 
 
+#include <AgilitySDK/d3dx12/d3dx12.h>
 #include <Core/CoreMinimal.hpp>
 #include <RHI/Types.hpp>
 #include <span>
@@ -17,32 +18,49 @@ namespace lde::RHI
 	{
 	public:
 		D3D12RootSignature() = default;
-		D3D12RootSignature(D3D12Device* pDevice,
-			const std::span<CD3DX12_ROOT_PARAMETER1>& Parameters,
-			const std::span<D3D12_STATIC_SAMPLER_DESC>& Samplers,
-			const D3D12_ROOT_SIGNATURE_FLAGS& RootFlags,
-			LPCWSTR DebugName = L"");
 		~D3D12RootSignature();
 	
-		void Create(D3D12Device* pDevice,
-			const std::span<CD3DX12_ROOT_PARAMETER1>& Parameters,
-			const std::span<D3D12_STATIC_SAMPLER_DESC>& Samplers,
-			const D3D12_ROOT_SIGNATURE_FLAGS& RootFlags,
-			LPCWSTR DebugName = L"");
-	
-		inline ID3D12RootSignature* GetRootSignature()
+		inline ID3D12RootSignature* Get()
 		{
 			return m_RootSignature.Get();
 		}
 	
-		PipelineType Type{};
-	
+		// Do as much Constants as possible; most efficient
+		void AddConstants(uint32 Count, uint32 RegisterSlot, uint32 Space = 0, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL);
+		// If possible, use instead of DescriptorTables
+		void AddCBV(uint32 RegisterSlot, uint32 Space = 0, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL);
+		// If possible, use instead of DescriptorTables
+		void AddSRV(uint32 RegisterSlot, uint32 Space = 0, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL);
+		// If possible, use instead of DescriptorTables
+		void AddUAV(uint32 RegisterSlot, uint32 Space = 0, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL);
+		// Least preferable; slowest performence
+		//void AddDescriptorTable(uint32 RegisterSlot, uint32 Space, std::span<D3D12_ROOT_DESCRIPTOR_TABLE D3D12_DESCRIPTOR_RANGE_TYPE Type, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL);
+
+		//
+		void AddStaticSampler(uint32 RegisterSlot, uint32 Space, 
+			D3D12_FILTER Filter, D3D12_TEXTURE_ADDRESS_MODE AddressMode, 
+			D3D12_COMPARISON_FUNC ComparsionFunc = D3D12_COMPARISON_FUNC_ALWAYS);
+
+		/**
+		 * @brief Note: when adding Paramters and StaticSamplers they are being pushed to the vector;
+		 * order of Parameters is from top-to-bottom.
+		 * @param pDevice 
+		 * @param eType Whether RootSignature is for Graphics or Compute state.
+		 * @param DebugName [Optional]
+		 * @return 
+		 */
+		HRESULT Build(D3D12Device* pDevice, PipelineType eType, std::string DebugName = "");
+		// TODO: Get Root from HLSL
+		//HRESULT Build(D3D12Device* pDevice, PipelineType eType, Shader* pShader);
+
 		void Release();
 	
+		PipelineType Type{};
 	private:
 		Ref<ID3D12RootSignature> m_RootSignature;
-	
-		void Serialize(D3D12Device* pDevice, const CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC& Desc, LPCWSTR DebugName);
-	
+
+		std::vector<D3D12_ROOT_PARAMETER1> m_Parameters;
+		std::vector<D3D12_STATIC_SAMPLER_DESC1> m_StaticSamplers;
+		
 	};
 } // namespace lde::RHI
