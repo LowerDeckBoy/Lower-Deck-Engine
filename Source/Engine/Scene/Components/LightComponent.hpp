416d0b963b9c06f1d9f868e4807ecd3203f861fa
LowerDeckBoy <pa.krawiec@onet.pl>
2024-06-26 11:49:45 +0200

    Improved lighting management.

--- Source/Engine/Scene/Components/LightComponent.hpp
+++ Source/Engine/Scene/Components/LightComponent.hpp
@@ -5,32 +5,71 @@
 */
 
 #include <DirectXMath.h>
+#include "RHI/D3D12/D3D12Texture.hpp"
 
 namespace lde
 {
 	using namespace DirectX;
 
+	// TODO:
+	// https://github.com/KatanaMajesty/WarpEngine/blob/master/src/World/Components/LightComponent.h
+	struct DirectionalLightShadowMap
+	{
+		XMMATRIX View;
+		XMMATRIX Projection;
+		XMMATRIX InvView;
+		XMMATRIX InvProjection;
+		RHI::D3D12RenderTexture Texture;
+		void Render(XMFLOAT3 Position);
+	};
+
 	struct DirectionalLightComponent
 	{
-		//XMFLOAT4 Position;
-		XMFLOAT4 Direction;
-		XMFLOAT4 Ambient = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-		XMFLOAT4 Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+		DirectionalLightComponent() = default;
+		DirectionalLightComponent(XMFLOAT3 Direction) { this->Direction = Direction; }
 
-		//bool bCastsShadows = false;
+		XMFLOAT3	Direction = XMFLOAT3(0.0f, 1.0f, 0.0f);
+		// Range [0.0, 1.0]
+		// 0.0 - not visible,
+		// 1.0 - fully visible
+		float		Visibility = 1.0f;
+		XMFLOAT4	Ambient = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+		XMFLOAT4	Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+		bool		bCastShadows = false;
+		XMFLOAT3	padding{};
+		
+		//DirectionalLightShadowMap ShadowMap;
+
 	};
 
 	struct PointLightComponent
 	{
-		XMFLOAT4 Position = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f); 
-		XMFLOAT4 Ambient = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-		float Range = 25.0f;
-		XMFLOAT3 padding = XMFLOAT3(0.0f, 0.0f, 0.0f);
+		PointLightComponent() = default;
+		PointLightComponent(XMFLOAT3 Position) { this->Position = Position; }
+
+		XMFLOAT3	Position = XMFLOAT3(0.0f, 1.0f, 0.0f); 
+		// Range [0.0, 1.0]
+		// 0.0 - not visible,
+		// 1.0 - fully visible
+		float		Visibility = 1.0f;
+		XMFLOAT4	Ambient = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+		float		Range = 25.0f;
+		XMFLOAT3	padding = XMFLOAT3(0.0f, 0.0f, 0.0f);
 	};
 
+	// TODO:
 	struct SpotLightComponent
 	{
 
+	};
+
+	
+
+	// TODO:
+	struct PointLightShadowMap
+	{
+		XMMATRIX Views[6];
+		XMMATRIX Projection;
 	};
 
 
