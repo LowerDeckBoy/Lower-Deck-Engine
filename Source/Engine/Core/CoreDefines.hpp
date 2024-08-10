8c44f725ffcd9c565a086f3544d834e6316dc9fe
LowerDeckBoy <pa.krawiec@onet.pl>
2024-08-10 19:51:47 +0200

    Added helper macros for quick enabling/disabling certain functionality.

--- Source/Engine/Core/CoreDefines.hpp
+++ Source/Engine/Core/CoreDefines.hpp
@@ -5,23 +5,18 @@
 	Definitions shared across Engine library content.
 */
 
-namespace lde
-{
-
 #define DEBUG_MODE (_DEBUG) || (DEBUG)
 
 #define VERSION_MAJOR "0"
 #define VERSION_MINOR "0"
 #define VERSION_PATCH "1"
-//#define ENGINE_VERSION std::string(VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_PATCH)
+
 #define ENGINE_VERSION std::format("{}.{}.{}", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 
-#if defined RHI_D3D12
-	constexpr const char* BACKEND = "D3D12";
-#else
-	constexpr const char* BACKEND = "Vulkan";
-#endif
+#define RAYTRACING		0
+#define MESH_SHADING	0
 
-#define RAYTRACING 1
+namespace lde
+{
 
 }
